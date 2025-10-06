7f2050a48d93b316a26275c7a1d46879b7008dfd
joseluis-02 <joseluis.c.jlgl@gmail.com>
2025-10-06 10:39:23 -0400

    Conversion del nombre de la empresa a mayúsculas

--- src/auth/components/CustomLogo.tsx
+++ src/auth/components/CustomLogo.tsx
@@ -4,7 +4,7 @@ export const CustomLogo = () => {
   return (
      <Link to="/" className="flex items-center whitespace-nowrap">
       <span className="font-montserrat font-bold text-xl m-0 whitespace-nowrap text-primary">
-       Guti<span className='text-black'>motos</span>
+       GUTI<span className='text-black'>MOTOS</span>
       </span>
       
      
