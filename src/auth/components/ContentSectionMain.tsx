35001f1eb00ecc47dbef0bcb1ee394f585abf28c
AbadBnz25 <benizsossaabad2@gmail.com>
2026-05-02 21:04:05 -0400

    minor change

--- src/auth/components/ContentSectionMain.tsx
+++ src/auth/components/ContentSectionMain.tsx
@@ -12,6 +12,8 @@ export const ContentSectionMain = () => {
   return (
     <>
       <div className="flex flex-col  min-h-[350px]  justify-between">
+
+        
         {section === 1 && <ContentSectionfirst handleSection={handleSection} />}
         {section === 2 && <ContentSectionSecond email={email} handleEmail={handleEmail} handleSection={handleSection} />}
         {section === 3 && <ContentSectionThird email={email} />}
